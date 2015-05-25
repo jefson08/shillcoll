3378bbcda410309cbbd7c1c9aecaa062c74d7ae0
root <root@DL02.scserver.edu>
2015-05-25 13:55:56 +0530

    Merge origin/master
    Conflicts:
    	nbproject/project.properties

---
+++ src/java/shg/report/report.java
@@ -0,0 +1,109 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package shg.report;
+
+import DBConnection.ConnectionPool;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.PrintWriter;
+import java.io.StringWriter;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.HashMap;
+import java.util.List;
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.ServletOutputStream;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import net.sf.jasperreports.engine.JRException;
+import net.sf.jasperreports.engine.JRResultSetDataSource;
+import net.sf.jasperreports.engine.JasperFillManager;
+import net.sf.jasperreports.engine.JasperRunManager;
+
+/**
+ *
+ * @author nic
+ */
+@WebServlet (name="report", urlPatterns={"/report"})
+public class report extends HttpServlet {
+    private Connection con = null;
+    private ResultSet rs = null;
+    private Statement s = null;
+    private ServletContext context = null;
+    private ConnectionPool connectionPool = null;
+    private String query = "";
+    
+   
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        
+        PrintWriter out = null;
+        
+        
+        
+        HashMap jasperParameter = new HashMap();
+        
+        
+        ServletOutputStream servletOutputStream = response.getOutputStream();
+        
+        InputStream reportStream = getServletConfig().getServletContext().getResourceAsStream("/report/report1.jasper");
+        
+        try {
+            context = getServletContext();
+            connectionPool = (ConnectionPool) context.getAttribute("ConnectionPool");
+            con = connectionPool.getConnection();
+        } catch (Exception e) {
+            System.out.println("Exception thrown by class " + this.getClass() + " at " + new java.util.Date() + " :: " + e);
+            con = null;
+            rs = null;
+            s = null;
+            context = null;
+            connectionPool = null;
+            return;
+        }
+        
+       
+        try{
+            query = "select rollno, studentname from studentdetails";
+            s = con.createStatement();
+            rs = s.executeQuery(query);
+            JRResultSetDataSource resultSetDataSource = new JRResultSetDataSource(rs);
+            
+            
+            JasperRunManager.runReportToPdfStream(reportStream, servletOutputStream, jasperParameter, resultSetDataSource);
+            //JasperRunManager.runReportToPdfStream(reportStream, servletOutputStream, new HashMap(), con);
+            s.close();
+            rs.close();
+            
+        }catch (Exception e){
+        // display stack trace in the browser
+            StringWriter stringWriter = new StringWriter();
+            PrintWriter printWriter = new PrintWriter(stringWriter);
+            e.printStackTrace(printWriter);
+            response.setContentType("text/plain");
+            response.getOutputStream().print(stringWriter.toString());
+        }
+        finally {
+            connectionPool.free(con);
+            con = null;
+            rs = null;
+            s = null;
+            context = null;
+            connectionPool = null;
+        }
+    }
+        
+}
+
+
